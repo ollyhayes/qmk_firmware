2d887ca2eb89f2b623c5382d3c92086ec47b8de2
Olly Hayes <ohayes@scottlogic.co.uk>
2018-11-14 13:21:26 +0000

    Add shift-F2 key for citrix

--- keyboards/ergodox_ez/keymaps/ollymap/keymap.c
+++ keyboards/ergodox_ez/keymaps/ollymap/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,     KC_W,    KC_F,    KC_P,    KC_G,    KC_FN0,
         KC_ESC,     KC_A,     KC_R,    KC_S,    KC_T,    KC_D,
         KC_LSFT,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_NUHS,
-        KC_LCTRL,   KC_LGUI,  KC_LALT, KC_NO,   KC_NUBS,
+        KC_LCTRL,   KC_LGUI,  KC_LALT, LSFT(KC_F2),   KC_NUBS,
                                                          KC_F6,   KC_F8,
                                                                   KC_PGUP,
                                                 KC_BSPC, KC_DEL,  KC_HOME,
