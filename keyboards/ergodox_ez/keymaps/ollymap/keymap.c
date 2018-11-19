946d84f18e0a577bf5b3973fe2c60bb970dafded
Olly Hayes <ohayes@scottlogic.co.uk>
2018-11-19 12:32:28 +0000

    Use dashes for transparent and fix some formatting

--- keyboards/ergodox_ez/keymaps/ollymap/keymap.c
+++ keyboards/ergodox_ez/keymaps/ollymap/keymap.c
@@ -11,14 +11,14 @@ typedef enum onoff_t {OFF, ON} onoff;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_GRV,     KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_F3,
-        KC_TAB,     KC_Q,     KC_W,    KC_F,    KC_P,    KC_G,    KC_FN0,
-        KC_ESC,     KC_A,     KC_R,    KC_S,    KC_T,    KC_D,
-        KC_LSFT,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_NUHS,
-        KC_LCTRL,   KC_LGUI,  KC_LALT, LSFT(KC_F2),   KC_NUBS,
-                                                         KC_F6,   KC_F8,
-                                                                  KC_PGUP,
-                                                KC_BSPC, KC_DEL,  KC_HOME,
+        KC_GRV,     KC_1,     KC_2,    KC_3,        KC_4,    KC_5,    KC_F3,
+        KC_TAB,     KC_Q,     KC_W,    KC_F,        KC_P,    KC_G,    KC_FN0,
+        KC_ESC,     KC_A,     KC_R,    KC_S,        KC_T,    KC_D,
+        KC_LSFT,    KC_Z,     KC_X,    KC_C,        KC_V,    KC_B,    KC_NUHS,
+        KC_LCTRL,   KC_LGUI,  KC_LALT, LSFT(KC_F2), KC_NUBS,
+                                                             KC_F6,   KC_F8,
+                                                                      KC_PGUP,
+                                                    KC_BSPC, KC_DEL,  KC_HOME,
         // right hand
         KC_F5,      KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
         KC_LBRC,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
@@ -32,23 +32,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // SYMBOLS
     [SYMB] = LAYOUT_ergodox(
        // left hand
-       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_FN1, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-               KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                               KC_INSERT,KC_TRNS,
-                                                       KC_TRNS,
-                                       KC_TRNS,KC_TRNS,KC_TRNS,
+       -------,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  -------,
+       -------,-------,-------,-------,-------,-------,-------,
+       KC_FN1, -------,-------,-------,-------,-------,
+       -------,-------,-------,-------,-------,-------,-------,
+               -------,-------,-------,-------,-------,
+                                               KC_INSERT,-------,
+                                                       -------,
+                                       -------,-------,-------,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,      KC_F11,
-       KC_TRNS, KC_TRNS, KC_7,   KC_8,    KC_9,    KC_KP_MINUS, KC_F12,
-                KC_TRNS, KC_4,   KC_5,    KC_6,    KC_KP_PLUS,  KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_1,   KC_2,    KC_3,    KC_KP_ENTER, KC_TRNS,
-                         KC_0,   KC_0,    KC_DOT,  KC_KP_ENTER, KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       -------, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,      KC_F11,
+       -------, -------, KC_7,   KC_8,    KC_9,    KC_KP_MINUS, KC_F12,
+                -------, KC_4,   KC_5,    KC_6,    KC_KP_PLUS,  -------,
+       -------, -------, KC_1,   KC_2,    KC_3,    KC_KP_ENTER, -------,
+                         KC_0,   KC_0,    KC_DOT,  KC_KP_ENTER, -------,
+       -------, -------,
+       -------,
+       -------, -------, -------
     )
 };
 
