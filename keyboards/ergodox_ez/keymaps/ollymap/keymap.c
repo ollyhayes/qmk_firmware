691b88c51129c7fd4001c3b81d0a7efd8cb806d1
Olly Hayes <ohayes@scottlogic.co.uk>
2018-11-19 13:00:16 +0000

    Switch to new thumb/hold num/func layout

--- keyboards/ergodox_ez/keymaps/ollymap/keymap.c
+++ keyboards/ergodox_ez/keymaps/ollymap/keymap.c
@@ -4,7 +4,10 @@
 #include "version.h"
 
 #define BASE 0 // default layer
-#define SYMB 1 // symbols
+#define NUMLEFT 1 // symbols
+#define NUMRIGHT 2 // symbols
+#define FUNCLEFT 3 // symbols
+#define FUNCRIGHT 4 // symbols
 
 typedef enum onoff_t {OFF, ON} onoff;
 
@@ -12,13 +15,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GRV,     KC_1,     KC_2,    KC_3,        KC_4,    KC_5,    KC_F3,
-        KC_TAB,     KC_Q,     KC_W,    KC_F,        KC_P,    KC_G,    KC_FN0,
+        KC_TAB,     KC_Q,     KC_W,    KC_F,        KC_P,    KC_G,    KC_F12,
         KC_ESC,     KC_A,     KC_R,    KC_S,        KC_T,    KC_D,
         KC_LSFT,    KC_Z,     KC_X,    KC_C,        KC_V,    KC_B,    KC_NUHS,
         KC_LCTRL,   KC_LGUI,  KC_LALT, LSFT(KC_F2), KC_NUBS,
                                                              KC_F6,   KC_F8,
                                                                       KC_PGUP,
-                                                    KC_BSPC, KC_DEL,  KC_HOME,
+                          //KC_BSPC, KC_DEL,  KC_HOME,
+                          LT(NUMLEFT, KC_BSPC), LT(FUNCLEFT,KC_DEL),  KC_HOME,
         // right hand
         KC_F5,      KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
         KC_LBRC,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
@@ -27,35 +31,102 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RGUI,
         KC_F10,     KC_F11,
         KC_PGDN,
-        KC_END,     KC_ENT,   KC_SPC
+        //KC_END,     KC_ENT,   KC_SPC
+        KC_END,     LT(FUNCRIGHT, KC_ENT),   LT(NUMRIGHT, KC_SPC)
     ),
-// SYMBOLS
-    [SYMB] = LAYOUT_ergodox(
+
+    [NUMLEFT] = LAYOUT_ergodox(
        // left hand
-       -------,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  -------,
-       -------,-------,-------,-------,-------,-------,-------,
-       KC_FN1, -------,-------,-------,-------,-------,
-       -------,-------,-------,-------,-------,-------,-------,
-               -------,-------,-------,-------,-------,
-                                               KC_INSERT,-------,
-                                                       -------,
-                                       -------,-------,-------,
+       -------,-------,-------,KC_KP_SLASH,KC_KP_ASTERISK,KC_KP_MINUS,-------,
+	   -------,-------,KC_KP_7,KC_KP_8,    KC_KP_9,       KC_KP_PLUS, -------,
+       -------,-------,KC_KP_4,KC_KP_5,    KC_KP_6,       KC_KP_PLUS,
+       -------,-------,KC_KP_1,KC_KP_2,    KC_KP_3,       KC_KP_ENTER,-------,
+       -------,-------,KC_KP_0,KC_KP_0,    KC_KP_DOT,
+                                                          -------,    -------,
+				                                                      -------,
+                                           -------,       -------,    -------,
+
        // right hand
-       -------, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,      KC_F11,
-       -------, -------, KC_7,   KC_8,    KC_9,    KC_KP_MINUS, KC_F12,
-                -------, KC_4,   KC_5,    KC_6,    KC_KP_PLUS,  -------,
-       -------, -------, KC_1,   KC_2,    KC_3,    KC_KP_ENTER, -------,
-                         KC_0,   KC_0,    KC_DOT,  KC_KP_ENTER, -------,
+       -------, -------, -------, -------, -------, -------, -------,
+       -------, -------, -------, -------, -------, -------, -------,
+                -------, -------, -------, -------, -------, -------,
+       -------, -------, -------, -------, -------, -------, -------,
+                         -------, -------, -------, -------, -------,
        -------, -------,
        -------,
        -------, -------, -------
-    )
+   ),
+   [NUMRIGHT] = LAYOUT_ergodox(
+       // left hand
+       -------,-------,-------,-------,-------,-------,-------,
+	   -------,-------,-------,-------,-------,-------,-------,
+       -------,-------,-------,-------,-------,-------,
+       -------,-------,-------,-------,-------,-------,-------,
+       -------,-------,-------,-------,-------,
+                                               -------,-------,
+                                                       -------,
+                                       -------,-------,-------,
+
+
+       -------,-------,-------,KC_KP_SLASH,KC_KP_ASTERISK,KC_KP_MINUS, -------,
+	   -------,-------,KC_KP_7,KC_KP_8,    KC_KP_9,       KC_KP_PLUS, -------,
+               -------,KC_KP_4,KC_KP_5,    KC_KP_6,       KC_KP_PLUS, -------,
+       -------,-------,KC_KP_1,KC_KP_2,    KC_KP_3,       KC_KP_ENTER,-------,
+                       KC_KP_0,KC_KP_0,    KC_KP_DOT,     KC_KP_ENTER,-------,
+       -------,-------,
+       -------,
+       -------,-------,-------
+    ),
+
+    [FUNCLEFT] = LAYOUT_ergodox(
+       // left hand
+       -------,-------,-------,-------,-------,-------,-------,
+	   -------,-------,KC_F7,  KC_F8,  KC_F9,  KC_F10, -------,
+       -------,-------,KC_F4,  KC_F5,  KC_F4,  KC_F11, 
+       -------,-------,KC_F1,  KC_F2,  KC_F3,  KC_F12, -------,
+       -------,-------,KC_F10, KC_F11, KC_F12,
+                                               -------,-------,
+                                                       -------,
+                                       -------,-------,-------,
+
+       // right hand
+       -------, -------, -------, -------, -------, -------, -------,
+       -------, -------, -------, -------, -------, -------, -------,
+                -------, -------, -------, -------, -------, -------,
+       -------, -------, -------, -------, -------, -------, -------,
+                         -------, -------, -------, -------, -------,
+       -------, -------,
+       -------,
+       -------, -------, -------
+   ),
+
+   [FUNCRIGHT] = LAYOUT_ergodox(
+       // left hand
+       -------,-------,-------,-------,-------,-------,-------,
+	   -------,-------,-------,-------,-------,-------,-------,
+       -------,-------,-------,-------,-------,-------,
+       -------,-------,-------,-------,-------,-------,-------,
+       -------,-------,-------,-------,-------,
+                                               -------,-------,
+                                                       -------,
+                                       -------,-------,-------,
+
+
+       -------,-------,-------,-------,-------,-------,-------,
+	   -------,KC_F10, KC_F7,  KC_F8,  KC_F9,  -------,-------,
+               KC_F11, KC_F4,  KC_F5,  KC_F4,  -------,-------,
+       -------,KC_F12, KC_F1,  KC_F2,  KC_F3,  -------,-------,
+                       KC_F10, KC_F11, KC_F12, -------,-------,
+       -------,-------,
+       -------,
+       -------,-------,-------
+    ),
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-    ACTION_LAYER_ON(SYMB,ON_RELEASE),           // FN1 - Enable Layer 1 (Symbols)
-    ACTION_LAYER_OFF(SYMB,ON_RELEASE)           // FN3 - Disable Layer 1 (Symbols)
-};
+//const uint16_t PROGMEM fn_actions[] = {
+//    // ACTION_LAYER_ON(SYMB,ON_RELEASE),           // FN1 - Enable Layer 1 (Symbols)
+//    // ACTION_LAYER_OFF(SYMB,ON_RELEASE)           // FN3 - Disable Layer 1 (Symbols)
+//};
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
