06f3152f23fe081a8642682bfc923e4e3c40c837
Olly Hayes <ohayes@scottlogic.co.uk>
2019-04-30 17:59:31 +0100

    WIP: Adding arrow layer

--- keyboards/ergodox_ez/keymaps/ollymap/keymap.c
+++ keyboards/ergodox_ez/keymaps/ollymap/keymap.c
@@ -6,6 +6,7 @@
 #define BASE 0 // default layer
 #define NUM 1 // symbols
 #define FUNCTIONS 2 // symbols
+#define ARR 3 // symbols
 
 typedef enum onoff_t {OFF, ON} onoff;
 
@@ -16,28 +17,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,     KC_W,    KC_F,        KC_P,    KC_G,    KC_NUBS,
         KC_ESC,     KC_A,     KC_R,    KC_S,        KC_T,    KC_D,
         KC_LSFT,    KC_Z,     KC_X,    KC_C,        KC_V,    KC_B,    KC_NUHS,
-        KC_LCTRL,   KC_LGUI,  KC_LALT, MO(NUM),     MO(FUNCTIONS),
+        KC_LCTRL,   KC_LGUI,  KC_LALT, MO(NUM),     KC_DEL,
                                                              KC_F12,  KC_F8,
                                                                       LSFT(KC_F2),
-                                                    KC_BSPC, KC_DEL,  KC_HOME,
+                                                    KC_BSPC, MO(ARR), KC_HOME,
         // right hand
         KC_F5,      KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
         KC_LBRC,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
                     KC_H,     KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
         KC_RBRC,    KC_K,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RGUI,
+                              MO(FUNC),KC_NO,   KC_RALT, KC_RGUI, KC_RCTRL,
         KC_F10,     KC_F11,
-        KC_PGDN,
+        KC_INS,
         KC_END,     KC_ENT,   KC_SPC
     ),
 
     [NUM] = LAYOUT_ergodox(
        // left hand
        RESET,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-	   KC_PGUP,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_PGDN,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+	   KC_PGUP,KC_TRNS,KC_KP_7,KC_KP_8,KC_KP_9,KC_TRNS,KC_TRNS,
+       KC_PGDN,KC_TRNS,KC_KP_4,KC_KP_5,KC_KP_6,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_KP_1,KC_KP_2,KC_KP_3,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_KP_0,KC_TRNS,KC_KP_DOT,  
                                                KC_TRNS,KC_TRNS,
                                                        KC_TRNS,
                                        KC_TRNS,KC_TRNS,KC_TRNS,
@@ -55,10 +56,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [FUNCTIONS] = LAYOUT_ergodox(
        // left hand
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-	   KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_PGDN,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+	   KC_TRNS,KC_TRNS,KC_F7,  KC_F8,  KC_F9,  KC_TRNS,KC_TRNS,
+       KC_PGDN,KC_TRNS,KC_F4,  KC_F5,  KC_F4,  KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_F10, KC_F11, KC_F12, 
                                                KC_TRNS,KC_TRNS,
                                                        KC_TRNS,
                                        KC_TRNS,KC_TRNS,KC_TRNS,
@@ -68,11 +69,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	   KC_TRNS,KC_F10, KC_F7,  KC_F8,  KC_F9,  KC_TRNS,KC_TRNS,
                KC_F11, KC_F4,  KC_F5,  KC_F4,  KC_TRNS,KC_TRNS,
        KC_TRNS,KC_F12, KC_F1,  KC_F2,  KC_F3,  KC_TRNS,KC_TRNS,
-                       KC_F10, KC_F11, KC_F12, KC_TRNS,KC_TRNS,
+                       KC_TRNS,KC_F11, KC_F12, KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,
        KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS
    ),
+
+    [ARR] = LAYOUT_ergodox(  // layer 0 : default
+        // left hand
+        KC_GRV,     KC_1,     KC_2,    KC_3,        KC_4,    KC_5,    KC_F3,
+        KC_TAB,     KC_Q,     KC_W,    KC_F,        KC_P,    KC_G,    KC_NUBS,
+        KC_ESC,     KC_A,     KC_R,    KC_S,        KC_T,    KC_D,
+        KC_LSFT,    KC_Z,     KC_X,    KC_C,        KC_V,    KC_B,    KC_NUHS,
+        KC_LCTRL,   KC_LGUI,  KC_LALT, MO(NUM),     KC_DEL,
+                                                             KC_F12,  KC_F8,
+                                                                      LSFT(KC_F2),
+                                                    KC_BSPC, MO(ARR), KC_HOME,
+        // right hand
+        KC_F5,      KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+        KC_LBRC,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
+                    KC_H,     KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+        KC_RBRC,    KC_K,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                              MO(FUNC),KC_NO,   KC_RALT, KC_RGUI, KC_RCTRL,
+        KC_F10,     KC_F11,
+        KC_INS,
+        KC_END,     KC_ENT,   KC_SPC
+    ),
 };
 
 //const uint16_t PROGMEM fn_actions[] = {
