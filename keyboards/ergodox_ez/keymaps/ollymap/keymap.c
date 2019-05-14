ad7b37625f49f2fcb7728c9f08d9525e38c70b2c
Olly Hayes <ohayes@scottlogic.co.uk>
2019-05-14 12:36:11 +0100

    Add punctuation layer

--- keyboards/ergodox_ez/keymaps/ollymap/keymap.c
+++ keyboards/ergodox_ez/keymaps/ollymap/keymap.c
@@ -9,6 +9,7 @@
 #define NUM 1 // symbols
 #define FUNCTIONS 2 // symbols
 #define ARR 3 // symbols
+#define PUNC 4 // symbols
 
 typedef enum onoff_t {OFF, ON} onoff;
 
@@ -25,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_GRV,     KC_1,     KC_2,    KC_3,        KC_4,    KC_5,    KC_F3,
         KC_TAB,     KC_Q,     KC_W,    KC_F,        KC_P,    KC_G,    KC_NUBS,
-        KC_ESC,     KC_A,     KC_R,    KC_S,        KC_T,    KC_D,
+        LT(PUNC,KC_ESC),     KC_A,     KC_R,    KC_S,        KC_T,    KC_D,
         KC_LSFT,    KC_Z,     KC_X,    KC_C,        KC_V,    KC_B,    KC_NUHS,
         KC_LCTRL,   KC_LGUI,  KC_LALT, MO(NUM),     KC_DEL,
                                                              KC_F12,  KC_F8,
@@ -34,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
         KC_F5,      KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
         KC_LBRC,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
-                    KC_H,     KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+                    KC_H,     KC_N,    KC_E,    KC_I,    KC_O,    MO(PUNC)
         KC_RBRC,    KC_K,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                               MO(FUNCTIONS),RAND,KC_RALT, KC_RGUI, KC_RCTRL,
         KC_F10,     KC_F11,
@@ -106,6 +107,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
     ),
+
+    [PUNC] = LAYOUT_ergodox(  // layer 0 : default
+        // left hand
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+	   KC_TRNS, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_TRNS,
+       KC_TRNS, S(KC_NUBS),S(KC_GRV),KC_LBRC,KC_RBRC,S(KC_NUHS),
+       KC_TRNS, KC_NUBS, KC_GRV,  KC_TRNS, KC_TRNS, KC_NUHS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                                    KC_TRNS, KC_TRNS,
+                                                             KC_TRNS,
+                                           KC_TRNS, KC_TRNS, KC_TRNS,
+
+        // right hand
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS,
+	   KC_TRNS, S(KC_6), S(KC_7), S(KC_8),       S(KC_9), S(KC_0), KC_TRNS,
+                S(KC_QUOT),S(KC_LBRC),S(KC_RBRC),S(KC_MINS),S(KC_EQL),KC_TRNS,
+       KC_TRNS, KC_QUOT, KC_TRNS, KC_TRNS,       KC_MINS, KC_EQL, KC_TRNS,
+                         KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+
+    //    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+	//    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  
+    //    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //                                                 KC_TRNS, KC_TRNS,
+    //                                                          KC_TRNS,
+    //                                        KC_TRNS, KC_TRNS, KC_TRNS,
+
+    //     // right hand
+    //    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    //    KC_TRNS, KC_TRNS,
+    //    KC_TRNS,
+    //    KC_TRNS, KC_TRNS, KC_TRNS
 };
 
 // Runs constantly in the background, in a loop.
