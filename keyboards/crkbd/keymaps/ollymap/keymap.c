0ae7ec74973005825c5100e6917098c34543f46e
Olly Hayes <ohayes@scottlogic.co.uk>
2019-07-18 12:59:39 +0100

    Can use KC_TRNS on qwerty layer

--- keyboards/crkbd/keymaps/ollymap/keymap.c
+++ keyboards/crkbd/keymaps/ollymap/keymap.c
@@ -72,13 +72,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT( \
   //,-----------------------------------------.                ,-----------------------------------------.
-        NUM_SWITCH,KC_Q,   KC_W,     KC_E,     KC_R,     KC_T,                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  MO(FUNCTIONS),\
+        KC_TRNS,     KC_Q,   KC_W,     KC_E,     KC_R,     KC_T,                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_TRNS,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-        KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                      KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_ENT,\
+        KC_TRNS,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                      KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_TRNS,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-       KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                      KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,\
+       KC_TRNS,      KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                      KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_TRNS,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                  KC_LCTRL,LT(PUNC, KC_DEL),LT(ARR, KC_BSPC),      KC_SPC, LT(PUNC, KC_INS), KC_RCTRL\
+                                  KC_TRNS, KC_TRNS,   KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS \
                               //`--------------------'  `--------------------'
   ),
 
