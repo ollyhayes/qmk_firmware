958387049f3180c13c55070ff92b5d939e81156f
Olly Hayes <olly.hayes@gmail.com>
2019-07-18 20:00:55 +0100

    Fix F10+ keys

--- keyboards/ergodox_ez/keymaps/ollymap/keymap.c
+++ keyboards/ergodox_ez/keymaps/ollymap/keymap.c
@@ -95,10 +95,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [FUNCTIONS] = LAYOUT_ergodox(
        // left hand
        RESET,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_F7,  KC_F8,  KC_F9,  KC_TRNS,KC_TRNS,
-       DF(QWERTY),KC_TRNS,KC_F4,  KC_F5,  KC_F4,  KC_TRNS,
-       DF(BASE),KC_TRNS,KC_F1,KC_F2, KC_F3,  KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_F10,KC_F11, KC_F12, 
+       KC_TRNS,KC_TRNS,KC_F7,  KC_F8,  KC_F9,  KC_F12,KC_TRNS,
+       DF(QWERTY),KC_TRNS,KC_F4,  KC_F5,  KC_F4,  KC_F11,
+       DF(BASE),KC_TRNS,KC_F1,KC_F2, KC_F3,  KC_F10,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                                KC_TRNS,KC_TRNS,
                                                        KC_TRNS,
                                        KC_TRNS,KC_TRNS,KC_TRNS,
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	   KC_TRNS,KC_F12, KC_F7,  KC_F8,  KC_F9,  KC_TRNS,KC_TRNS,
                KC_F11, KC_F4,  KC_F5,  KC_F4,  KC_TRNS,KC_TRNS,
        KC_TRNS,KC_F10, KC_F1,  KC_F2,  KC_F3,  KC_TRNS,KC_TRNS,
-                       KC_TRNS,KC_F11, KC_F12, KC_TRNS,KC_TRNS,
+                       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,
        KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS
