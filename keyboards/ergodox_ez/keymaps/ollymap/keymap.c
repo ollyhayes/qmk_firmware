13a7ce3663730067c28b67544184b907bc6c596b
Olly Hayes <ohayes@scottlogic.co.uk>
2019-07-18 12:29:04 +0100

    Remove easymap

--- keyboards/ergodox_ez/keymaps/ollymap/keymap.c
+++ keyboards/ergodox_ez/keymaps/ollymap/keymap.c
@@ -6,7 +6,7 @@
 #include "words.c"
 
 #define BASE 0 // default layer
-#define EASY 1 // default layer
+// #define EASY 1 // default layer
 #define NUM 2 // symbols
 #define FUNCTIONS 3 // symbols
 #define ARR 4 // symbols
@@ -30,7 +30,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_NO,      KC_NO,    KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_F3,
+        KC_GRV,     KC_1,     KC_2,    KC_3,        KC_4,    KC_5,    KC_F3,
         NUM_SWITCH, KC_Q,     KC_W,    KC_F,        KC_P,    KC_G,    KC_NO,
         KC_ESC,     KC_A,     KC_R,    KC_S,        KC_T,    KC_D,
         KC_LSFT,    KC_Z,     KC_X,    KC_C,        KC_V,    KC_B,    KC_DEL,
@@ -39,36 +39,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                       LSFT(KC_F2),
                                                     KC_BSPC, MO(ARR), KC_HOME,
         // right hand
-        KC_F5,      KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_F5,      KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
         KC_NO,      KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN, MO(FUNCTIONS),
                     KC_H,     KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
         KC_NO,      KC_K,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                              MO(PUNC),KC_RCTRL,TG(EASY),KC_RGUI,KC_RCTRL,
+                              MO(PUNC),KC_RCTRL,RAND,    KC_RGUI,KC_RCTRL,
         KC_F10,     KC_F11,
         KC_INS,
         KC_END,     KC_ENT,   KC_SPC
     ),
 
-    [EASY] = LAYOUT_ergodox(  // layer 0 : default
-        // left hand
-        KC_GRV,     KC_1,     KC_2,    KC_3,        KC_4,    KC_5,    KC_F3,
-        NUM_SWITCH, KC_Q,     KC_W,    KC_F,        KC_P,    KC_G,    KC_NUBS,
-        KC_ESC,     KC_A,     KC_R,    KC_S,        KC_T,    KC_D,
-        KC_LSFT,    KC_Z,     KC_X,    KC_C,        KC_V,    KC_B,    KC_NUHS,
-        KC_LCTRL,   KC_LGUI,  KC_LALT, KC_DEL,      MO(PUNC),
-                                                             KC_F12,  KC_F8,
-                                                                      LSFT(KC_F2),
-                                                    KC_BSPC, MO(ARR), KC_HOME,
-        // right hand
-        KC_F5,      KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-        KC_LBRC,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN, MO(FUNCTIONS),
-                    KC_H,     KC_N,    KC_E,    KC_I,    KC_O,    KC_NO,
-        KC_RBRC,    KC_K,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                              MO(PUNC),RAND,    TG(EASY),KC_RGUI, KC_RCTRL,
-        KC_F10,     KC_F11,
-        KC_INS,
-        KC_END,     KC_ENT,   KC_SPC
-    ),
+    // [EASY] = LAYOUT_ergodox(  // layer 0 : default
+    //     // left hand
+    //     KC_GRV,     KC_1,     KC_2,    KC_3,        KC_4,    KC_5,    KC_F3,
+    //     NUM_SWITCH, KC_Q,     KC_W,    KC_F,        KC_P,    KC_G,    KC_NUBS,
+    //     KC_ESC,     KC_A,     KC_R,    KC_S,        KC_T,    KC_D,
+    //     KC_LSFT,    KC_Z,     KC_X,    KC_C,        KC_V,    KC_B,    KC_NUHS,
+    //     KC_LCTRL,   KC_LGUI,  KC_LALT, KC_DEL,      MO(PUNC),
+    //                                                          KC_F12,  KC_F8,
+    //                                                                   LSFT(KC_F2),
+    //                                                 KC_BSPC, MO(ARR), KC_HOME,
+    //     // right hand
+    //     KC_F5,      KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+    //     KC_LBRC,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN, MO(FUNCTIONS),
+    //                 KC_H,     KC_N,    KC_E,    KC_I,    KC_O,    KC_NO,
+    //     KC_RBRC,    KC_K,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    //                           MO(PUNC),RAND,    TG(EASY),KC_RGUI, KC_RCTRL,
+    //     KC_F10,     KC_F11,
+    //     KC_INS,
+    //     KC_END,     KC_ENT,   KC_SPC
+    // ),
 
     [NUM] = LAYOUT_ergodox(
        // left hand
